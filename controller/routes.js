8174a607dc70b431c1d64aa13ac20957f0eaaab2
Chris Bragg <cbragg9@yahoo.com>
2020-05-29 16:01:00 -0700

    handlebar display from sql database

--- controller/routes.js
+++ controller/routes.js
@@ -6,8 +6,24 @@ const { Op } = require("sequelize");
 
 router.get("/", function (req, res) {
     Inventory.findAll().then(function(data) {
-        console.log(data);
-        res.render("index");
+        let dataArray = [];
+
+        for (var i = 0; i < data.length; i++) {
+            let productEntry = {
+                id: data[i].ID,
+                name: data[i].name,
+                price: data[i].price,
+                stock: data[i].stock,
+                image: data[i].image
+            }
+            dataArray.push(productEntry);
+        }
+
+        let hbsObject = {
+            product: dataArray
+        };
+        console.log(hbsObject)
+        res.render("index", hbsObject);
     })
 })
 
